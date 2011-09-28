87f3f2af8ad68caa98b621057c0b92f60ec93152
jpfoley <jfoley@physics.utah.edu>
2011-09-28 16:57:07 -0400

    Add oprod class definitions to hisq_force_utils.h

--- include/hisq_force_utils.h
+++ include/hisq_force_utils.h
@@ -12,6 +12,49 @@ extern "C" {
   namespace hisq{
     namespace fermion_force{
 
+      typedef struct  {
+        size_t bytes;
+        QudaPrecision precision;
+        int length; // total length
+        int volume; // geometric volume (single parity)
+        int X[4];   // the geometric lengths (single parity)
+        int Nc; // length of color dimension
+        void *data; // either (double2*) or (float2*) or (float4*)
+      } ParityMatrix;
+
+      typedef struct {
+        ParityMatrix odd;
+        ParityMatrix even;
+      } FullMatrix;
+
+      typedef struct {
+        ParityMatrix odd;
+        ParityMatrix even;
+      } FullCompMatrix; // compressed matrix 
+
+
+      typedef struct {
+        size_t bytes;
+        QudaPrecision precision;
+        int length; // total length
+        int volume; // geometric volume
+        int X[4];
+        int Nc; // length of color dimension
+        void *data[8]; // array of 8 pointers (Not a pointer to an array!) 
+      } ParityOprod;
+
+
+  typedef struct {
+    ParityOprod odd;
+    ParityOprod even;
+  } FullOprod;
+
+
+
+
+
+
+
       void loadOprodToGPU(void *cudaOprodEven, void *cudaOprodOdd, void *cpuOprod, int vol);
       void allocateOprodFields(void **cudaOprodEven, void **cudaOprodOdd, int vol);
       void fetchOprodFromGPU(void *cudaOprodEven, void *cudaOprodOdd, void *cpuOprod, int vol);
